4054b0c63f18abdaffaeac531e9a37fc86cc8a10
JakeServin <jakeservin@gmail.com>
2022-08-01 23:59:41 -0700

    added final build

---
+++ client/src/components/Chat/Chat.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import "./Chat.css";
+import {useHistory} from 'react-router-dom';
+
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
+import TextContainer from '../TextContainer/TextContainer'
+
+let socket;
+
+export default function Chat({ location }) {
+	const [name, setName] = useState("");
+	const [room, setRoom] = useState("");
+	const [users, setUsers] = useState("");
+	const [message, setMessage] = useState("");
+	const [messages, setMessages] = useState([]);
+  	const ENDPOINT = "https://jservin-react-chat-app.herokuapp.com";
+  	const history = useHistory()
+
+	useEffect(() => {
+		const { name, room } = queryString.parse(location.search);
+
+		socket = io(ENDPOINT);
+
+		setName(name);
+		setRoom(room);
+
+    socket.emit("join", { name, room }, (error) => {
+      if(error) {    
+        alert(error)
+        history.push("/");
+      }
+    });
+    
+		return function cleanUp() {
+			socket.emit("disconnect");
+			socket.off();
+		};
+	}, [ENDPOINT, location.search]);
+
+	useEffect(() => {
+		socket.on("message", (message) => {
+			setMessages([...messages, message]);
+		});
+		socket.on('roomData', ({ users }) => {
+			setUsers(users)
+		})
+	}, [messages]);
+
+	// function for sending messages
+
+	const sendMessage = (event) => {
+		event.preventDefault();
+
+		if (message) {
+			socket.emit("sendMessage", message, () => setMessage(""));
+		}
+	};
+	console.log(message, messages);
+
+	return (
+		<div className="outerContainer">
+			<div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+				<Input
+					message={message}
+					setMessage={setMessage}
+					sendMessage={sendMessage}
+				/>
+			</div>
+		<TextContainer users={users} />
+		</div>
+	);
+}
