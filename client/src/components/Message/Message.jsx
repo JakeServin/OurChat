6b37bb928907ae50231a0951b28cd8c945fdd7d3
JakeServin <jakeservin@gmail.com>
2022-08-02 11:12:08 -0700

    added mockup image

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -17,13 +17,17 @@ export default function Message({ message: {user, text}, name }) {
 		<div className="messageContainer justifyEnd">
 			<p className="sentText pr-10">{trimmedName}</p>
 			<div className="messageBox backgroundBlue">
-				<p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
+				<p className="messageText colorWhite">
+					{ReactEmoji.emojify(text)}
+				</p>
 			</div>
 		</div>
 	) : (
 		<div className="messageContainer justifyStart">
 			<div className="messageBox backgroundLight">
-				<p className="messageText colorDark">{text}</p>
+				<p className="messageText colorDark">
+					{ReactEmoji.emojify(text)}
+				</p>
 			</div>
 			<p className="sentText pl-10">{user}</p>
 		</div>
